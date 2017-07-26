52dd5cf14e9aa1ae9f7603c8fdc22321be1fdc4a
Markus Tuominen <markus.tuominen91@gmail.com>
2017-07-26 18:29:23 +0300

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {render} from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return ( // return voi palauttaa vain yhden parent-elementin
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Heippa!</h1>
+                    </div>
+                </div>
+            </div>                
+        );
+    }
+}
+
+render(<App/>, window.document.getElementById("app"));
