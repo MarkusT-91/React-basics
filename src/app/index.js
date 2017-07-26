a99af2622ed3ee0562d5f0b3f5d7b71f930efb06
Markus Tuominen <markus.tuominen91@gmail.com>
2017-07-26 18:52:50 +0300

    tutorial 4

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {render} from "react-dom";
 
+import {Header} from "./components/Header";
+import {Home} from "./components/Home";
+
 class App extends React.Component {
     render() {
         return ( // return voi palauttaa vain yhden parent-elementin
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <h1>Heippa!</h1>
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home/>
                     </div>
                 </div>
             </div>                
