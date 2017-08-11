6ba6faf2deabb89dcea7e2c6edf69bd827a47adc
Markus Tuominen <markus.tuominen91@gmail.com>
2017-08-11 14:14:12 +0300

    tutorial 15
    route setup

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,103 +1,11 @@
 import React from "react";
-import PropTypes from 'prop-types'; // PropTypes toimi ennen perus-React paketin kanssa, mutta nykyään pitää importata erikseen PropTypes
 
 export class Home extends React.Component {
-
-    constructor(props) {
-        super();
-        this.state = {
-            age: props.initialAge,
-            status: 0,
-            homeLink: props.initialLinkName
-        };
-        setTimeout(() => {
-            this.setState({
-                status: 1
-            });
-        },3000);
-        console.log("Constructor");
-    }
-
-    componentWillMount() {
-        console.log("Component will mount");
-    }
-
-    componentDidMount() {
-        console.log("Component did mount");
-    }
-
-    componentWillReceiveProps(nextProps) {
-        console.log("Component will receive props", nextProps);
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("Should component update", nextProps, nextState);
-        // if(nextState.status === 1) {
-        //     return false;
-        // }
-        return true;
-    }
-
-    componentWillUpdate(nextProps, nextState) {
-        console.log("Component will update", nextProps, nextState);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        console.log("Component did update", prevProps, prevState);
-    }
-
-    componentWillUnmount() {
-        console.log("Component will unmount");
-    }
-
-    onMakeOlder() {
-        this.setState({
-            age: this.state.age + 3
-        });
-    }
-
-    onChangeLink() {
-        this.props.changeLink(this.state.homeLink);
-    }
-
-    onMakeYounger() {
-        this.setState({
-            age: this.state.age - 3       
-        });
-        if(this.state.age <= 0) {
-                alert("Lopeta!");
-            }
-    }
-    
-    onHandleChange(event) {
-        this.setState({
-            homeLink: event.target.value
-        });
-    }
-
     render() {
-        var text = "Jotain tekstiä!";
         return (
             <div>
-                <p>Uusi komponentti!</p>       
-                <p>Nimesi on {this.props.name}, ikäsi on {this.state.age}</p>
-                <p>Status: {this.state.status}</p>
-                <hr/> 
-                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Lisää ikää!</button>
-                <button onClick={() => this.onMakeYounger()} className="btn btn-danger">Vähennä ikää!</button>
-                <hr/>
-                <button onClick={this.props.greet} className="btn btn-success">Tervehdi</button>
-                <hr/>
-                <input type="text" value={this.state.homeLink} onChange={(event) => this.onHandleChange(event)}/>  
-                <button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Vaihda linkin nimi</button>
+                <h3>Home</h3>
             </div>
-        );
+        )
     }
 }
-
-Home.propTypes = { // määrittää mitä tyyppiä minkäkin arvon tulee olla. Jollei esim nimi ole string = error
-    name: PropTypes.string,
-    age: PropTypes.number,
-    greet: PropTypes.func,
-    initialLinkName: PropTypes.string
-};
--- src/app/index.js
+++ src/app/index.js
@@ -1,66 +1,28 @@
 import React from "react";
 import {render} from "react-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 import {Header} from "./components/Header";
 import {Home} from "./components/Home";
+import {Root} from "./components/Root";
+import {User} from "./components/User";
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            homeLink: "Home",
-            homeMounted: true
-        };
-    }
-
-    onGreet() {
-        alert("Heippa!");
-    }
-
-    onChangeLinkName(newName) {
-        this.setState({
-            homeLink: newName
-        });
-    }
-
-    onChangeHomeMounted() {
-        this.setState({
-            homeMounted: !this.state.homeMounted
-        });
-    }
-
     render() {
-        let homeCmp = "";
-        if(this.state.homeMounted) {
-            homeCmp = (
-                <Home 
-                name={"Markus"} 
-                initialAge={26} 
-                greet={this.onGreet}
-                changeLink={this.onChangeLinkName.bind(this)}
-                initialLinkName={this.state.homeLink}
-                />
-            );
-        }
-        return ( // return voi palauttaa vain yhden parent-elementin
-            <div className="container">
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                        <Header homeLink={this.state.homeLink}/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                       {homeCmp}                                              
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1">
-                       <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount Home Component</button>                                             
-                    </div>
-                </div>
-            </div>                
-        );
+        return (
+           <Router>
+               <div>
+                   <Root>
+                       <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path={"/user"} component={User} />
+                            <Route path={"/home"} omponent={Home} />
+                       </Switch>
+                   </Root>
+               </div>              
+           </Router>
+        ) ;
+        
     }
 }
 
